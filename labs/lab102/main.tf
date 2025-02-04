da34e7b1a559eb91b15715d39ea3f1dc84a008e5
Your Name <you@example.com>
2025-02-04 13:47:56 +0000

    lab102 Adding time provider and using a new resource named time_sleep to wait 10s for the ip_address

---
+++ labs/lab102/main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    time = {
+      source = "hashicorp/time"
+      version = "0.12.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+resource "time_sleep" "wait_for_ip" {
+  create_duration = "10s"  # Wait for 10 seconds
+}
+
+resource "aws_security_group" "sg" {
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "vm" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.sg.id]
+
+  tags = {
+    Name = "kobi-vm"
+  }
+}
+
+output "vm_public_ip" {
+  value       = aws_instance.vm.public_ip
+  depends_on  = [time_sleep.wait_for_ip]  # Wait for the time_sleep resource to complete
+  description = "Public IP address of the VM"
+
+}
