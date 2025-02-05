ba2ed3b601b7833257d52d628a5fb24d72c78826
Your Name <you@example.com>
2025-02-05 09:41:56 +0000

    lab101 is updated according to lab102 cause its the main file to work with

--- labs/lab101/main.tf
+++ labs/lab101/main.tf
@@ -1,9 +1,41 @@
+terraform {
+  required_providers {
+    time = {
+      source = "hashicorp/time"
+      version = "0.12.1"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
 variable "region" {
   default = "us-east-1"
+}
+
+resource "time_sleep" "wait_for_ip" {
+  create_duration = "10s"  # Wait for 10 seconds
+}
+
+resource "null_resource" "run_script" {
+  provisioner "local-exec" {
+    command = "echo 'Hello Jb Class'"
+  }
+}
+
+resource "null_resource" "check_public_ip" {
+    provisioner "local-exec" {
+    command = <<EOT
+      if [ -z "${aws_instance.vm.public_ip}" ]; then
+        echo "ERROR: Public IP address was not assigned." >&2
+        exit 1
+      fi
+    EOT
+  }
+  
+  depends_on = [ aws_instance.vm ]
 }
 
 resource "aws_security_group" "sg" {
@@ -35,5 +67,8 @@ resource "aws_instance" "vm" {
 
 output "vm_public_ip" {
   value       = aws_instance.vm.public_ip
+  depends_on  = [null_resource.check_public_ip]  # Wait for the time_sleep resource to complete
+  # depends_on = [ time_sleep.wait_for_ip] # Wait for the time_sleep resource to complete
   description = "Public IP address of the VM"
+
 }
--- labs/lab102/main.tf
+++ labs/lab102/main.tf
@@ -68,6 +68,7 @@ resource "aws_instance" "vm" {
 output "vm_public_ip" {
   value       = aws_instance.vm.public_ip
   depends_on  = [null_resource.check_public_ip]  # Wait for the time_sleep resource to complete
+  # depends_on = [ time_sleep.wait_for_ip] # Wait for the time_sleep resource to complete
   description = "Public IP address of the VM"
 
 }
