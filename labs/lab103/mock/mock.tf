392c6e936eb9929d376c23d17f00baea22575ca6
Your Name <you@example.com>
2025-02-05 14:31:25 +0000

    Updating mock to show 2 ways to get AMI id without the ID

--- labs/lab103/mock/mock.tf
+++ labs/lab103/mock/mock.tf
@@ -15,6 +15,22 @@ variable "region" {
   default = "us-east-1"
 }
 
+# 2 ways to find an AMI id without having the ID
+data "aws_ami" "terraformami" {
+  owners = ["self"]
+  filter {
+    name = "name"
+    values = ["terraform-workshop-image-do-not-delete"]
+  }
+  
+}
+
+data "aws_ami" "my-privateami" {
+    owners = ["self"]  # Queries only AMIs owned by your account
+
+}
+
+
 # Create a var for the instance_id given to us
 variable "instance_id" {
   description = "The ID of the EC2 instance"
@@ -31,4 +47,13 @@ data "aws_instance" "yaniv_vm" {
 output "aws_yaniv_vm_public_ip" {
   value       = data.aws_instance.yaniv_vm.public_ip
   description = "Public IP of the specified instance"
+}
+
+# 2 ways Output
+output "yanivsami" {
+  value = data.aws_ami.my-privateami
+}
+
+output "terraformimage" {
+  value = data.aws_ami.terraformami
 }
