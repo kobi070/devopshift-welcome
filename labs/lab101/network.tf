e2e2c0c9849db8a4df10a439a775a790d880d416
Your Name <you@example.com>
2025-02-06 07:18:36 +0000

    Updated lab101 to get all the files from lab103 as its the main project folder

---
+++ labs/lab101/network.tf
@@ -0,0 +1,27 @@
+# Create a aws security group
+# Open port 22 for ssh ingress=inbound rules and egress=outbound rules
+# Should open port 80
+
+resource "aws_security_group" "sg" {
+ ingress {
+   from_port   = 22
+   to_port     = 22
+   protocol    = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+
+   ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+ egress {
+   from_port   = 0
+   to_port     = 0
+   protocol    = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+}
