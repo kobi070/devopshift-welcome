fdeec2a08a3dfec68a6bf390bed16caadd947847
kobi070 <kobi070@gmail.com>
2025-02-10 10:53:29 +0200

    Initial commit of py_labs contains all labs in our python course in branch workshop/python

---
+++ py_labs/lab101/lab101.py
@@ -0,0 +1,30 @@
+# Create a list or dict of objects (like servers list)
+# Use input() function to get a certain number from  the user and check if its exists in the list or not (should raise an exception OutOfBoundsException or etc)
+# Use try/except block to catch the exception and print a message to the user
+
+# Create a list of servers
+list_servers = ['apache2', 'nginx', 'docker']
+dict_servers = {"apache2": True, "nginx": True, "docker": True}
+
+#  User input
+user_input = input("Enter a service: ")
+user_input2 = input("Enter a service: ")
+
+try:
+       print(user_input in list_servers)
+except ValueError as error:
+        print(f"{user_input} is invalid, {error.with_traceback()}")
+
+
+try:
+    print(dict_servers[user_input2])
+    if dict_servers[user_input] is not True:
+          print(f"{user_input} is not running")
+    else:
+            print(f"{user_input} is running")
+          
+except KeyError as error:
+    print(f"{user_input} is invalid, {error.with_traceback()}")
+
+
+val = list_servers.index(user_input) ? print("works") : 
