7fdcb69f658434a86a381a9129cea7fb7dbeeccf
kobi070 <kobi070@gmail.com>
2025-02-11 15:10:25 +0200

    Updated version of tools.py to add a function called add_server which checks if a certain server exists in our dict and if not add it

--- py_labs/lab102/tools.py
+++ py_labs/lab102/tools.py
@@ -8,11 +8,18 @@ def check_service_status(server_name: str) -> bool:
         if(server_dictionary[server_name]):
             logger.info(f"{server_name} is running")
             return server_dictionary[server_name]
-        else:
-            logger.info(f"{server_name} is not running")
-            return server_dictionary[server_name]
     except KeyError:
         logger.error(f"An error has occured: {server_name} is not supported")
+    logger.info(f"{server_name} is not running")
+    return server_dictionary[server_name]
+
+def add_server(server_name: str, is_running: bool) -> bool:
+    try:
+        if(server_name not in server_dictionary.keys()):
+            server_dictionary[server_name] = is_running
+            return True
+    except KeyError:
+        logger.error(f"{server_name} is not a valid name server")
     return False
 
 def check_server_status():
