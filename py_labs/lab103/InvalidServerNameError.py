0015134919353492277bada308583ce91dd1e98f
kobi070 <kobi070@gmail.com>
2025-02-12 09:09:46 +0200

    Addition of Server validation error

---
+++ py_labs/lab103/InvalidServerNameError.py
@@ -0,0 +1,7 @@
+class InvalidServerNameError(Exception):
+    pass
+
+
+# def check_server_name(server_name):
+#     if server_name:
+#         raise InvalidServerNameError("Invalid server name ! cannot be empty")
