95b6dcd9f91823eacdacd221877bf2a1cae38515
Kobi <kobi070@gmail.com>
2025-02-27 13:58:08 +0200

    Updated files

--- demos/my-shared-library/vars/myLibrary.groovy
+++ demos/my-shared-library/vars/myLibrary.groovy
@@ -2,15 +2,19 @@
 
 def buildApp() {
     echo "Now building the application..."
+    echo "buildApp function is called from myLibrary.groovy"
     // Mock build logic here
 }
 
 def deployApp(String branchName) {
     echo "Now deploying the application on branch: ${branchName}..."
+    echo "deployApp function is called from myLibrary.groovy"
     // Mock deploy logic here
 }
 
 def cleanup() {
     echo "Cleaning up after build and deployment..."
+    echo "Cleanup function is called from myLibrary.groovy"
+    
     // Mock cleanup logic here
 }
