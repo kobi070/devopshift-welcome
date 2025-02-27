4a8e3d68b7eb84b3ece43296ec390158703f06fb
Kobi <kobi070@gmail.com>
2025-02-27 14:09:53 +0200

    Addition of a few functions to my .groovy and Updated version of Jenkinsfile to use those functions

--- demos/my-shared-library/vars/myLibrary.groovy
+++ demos/my-shared-library/vars/myLibrary.groovy
@@ -18,3 +18,21 @@ def cleanup() {
     
     // Mock cleanup logic here
 }
+
+def whichBranch(String branchName) {
+    echo "Checking out branch: ${branchName}..."
+    echo "whichBranch function is called from myLibrary.groovy"
+    // Mock branch checking logic here
+}
+
+def getVersion() {
+    echo "Getting the version number..."
+    echo "getVersion function is called from myLibrary.groovy"
+    // Mock version retrieval logic here
+}
+
+def testApp(){
+    echo "Running tests..."
+    echo "testApp function is called from myLibrary.groovy"
+    // Mock test running logic here
+}
