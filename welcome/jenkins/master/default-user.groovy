e35ed34c7d56106edb096d95f86c69c2a0789ebd
KobiK <kobi@rafael.co.il>
2025-04-28 15:09:19 +0300

    fixing no comments on .groovy

--- welcome/jenkins/master/default-user.groovy
+++ welcome/jenkins/master/default-user.groovy
@@ -3,12 +3,23 @@ import hudson.security.*
 
 def env = System.getenv()
 
-def jenkins = Jenkins.getInstance()
-jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
-jenkins.setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy())
+// Get the Jenkins instance
+def jenkins = Jenkins.get()
 
-def user = jenkins.getSecurityRealm().createAccount(env.JENKINS_USER, env.JENKINS_PASS)
+// Set the security realm (authentication method)
+def securityRealm = new HudsonPrivateSecurityRealm(false)
+jenkins.setSecurityRealm(securityRealm)
+
+// Set the authorization strategy (permissions)
+def authorizationStrategy = new GlobalMatrixAuthorizationStrategy()
+jenkins.setAuthorizationStrategy(authorizationStrategy)
+
+// Create the user account with the provided environment variables
+def user = securityRealm.createAccount(env.JENKINS_USER, env.JENKINS_PASS)
 user.save()
 
-jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, env.JENKINS_USER)
+// Grant the newly created user admin permissions
+authorizationStrategy.add(Jenkins.ADMINISTER, env.JENKINS_USER)
+
+// Save the Jenkins instance configuration
 jenkins.save()
